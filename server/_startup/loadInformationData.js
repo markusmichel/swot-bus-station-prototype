bbd39a397c2a8c6b3fa982a2eb98fdf99781143d
Noodlewood <daniel.riedmuller@gmail.com>
2015-05-07 14:15:06 +0200

    profileImage included

--- server/_startup/loadInformationData.js
+++ server/_startup/loadInformationData.js
@@ -11,6 +11,7 @@ if(information.length < 1) {
         "device": {
             "id": "SWOTY Prototype",
             "registered": false,
+            "profileimage": "http://th03.deviantart.net/fs41/PRE/f/2009/029/7/9/Mr__Grimm_aka_The_Thing_by_mwof.jpg",
             "api": {
                 "url": "http://192.168.178.22:3000/api"
             },
